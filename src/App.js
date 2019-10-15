98b5cda86b338cd918c3e2f6937ead8cfc79bd69
rroddone <ricardoromero100@hotmail.com>
2019-10-15 00:31:35 -0300

    commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Ricky from './main';
+
+function App() {
+  return (
+    <div className="App">
+     <Ricky/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.js
@@ -0,0 +1,80 @@
+import React from 'react';
+
+class Main extends React.Component{
+    render(){
+        return(
+          <>
+          <header>
+            <div class="cabezera">
+            <h2>Mis pasiones</h2>
+          </div>
+          </header>
+
+          <body>
+          <div class="derecha" align="center">
+            <div class="rightcolumn">
+              <div class="card">
+                <h2>About Me</h2>
+                <p>como sabran, sino es cocinar, la cosa que mas me gusta hacer es entrenar, aparte de eso, paso mis dias en la UPA. Mucha gente, incluyendo 
+                    algunos miembros de mi familia dicen que no se priorizar entre lo que es el estudio universitario y el entrenamiento.
+                    Yo les digo que hay una linea delgada entre lo que es la adiccion y la pasion. Como atleta amateur, tengo que entrenar en una base diaria
+                    pero como alumno universiatario, tambien tengo que hacer los trabajos de la facultad, es el gran problema que cada atleta universitario debe de pasar
+                    lastimosamente. Y una vez que haya terminado la universidad, quisiera poder llegar a estudiar cocina en alguna escuela en Francia.
+                </p>
+              </div>
+              <div class="card2">
+                <h2>Mi vida deportiva</h2>
+                <p>Todo comenzo hace solamente 3 años a finales del 2016, donde decidi  dar un cambio a mi vida sedentaria a una vida totalmente
+                  diferente a lo que era antes, eso tambien incluía la alimentacion. Fue un comienzo muuuy bruzco, pero, todo trabajo duro da sus recompensas.
+                  Puede que haya momentos en donde uno quiere tirar la toalla y no volver más, pero, es ese dolor y sufrimiento que hace cambiar a las personas
+                  para lo mejor, es ese esfuerzo que diferencia de los que llegan a cumplir sus metas de aquellos que esperan a que pase solo las cosas.
+                  No hay pers enona este mundo que puede dar el primer en lugar de alguien, el señor y dueño de tu destino es tu propia persona, si queres
+                  seguir adelante, da el primer paso.
+                </p>
+              </div>
+              </div>
+              </div> 
+          <div class="izquierda" align="center">
+            <div class="leftcolumn" align="center">
+                <h2>Mi blog personal</h2>
+                <h5>Mi entrenamiento</h5>
+                <div class="row2">
+                  <img src="https://i.pinimg.com/originals/38/5b/e5/385be5fd940d6ecbacb5cee698da8ece.jpg" class="cross"/>
+                 </div>
+                <p>todos los dias (de lunes a sabado) paso entrenando varias actividades deportivas a lo largo del día. Lo cual inluye levantamiento olimpico,
+                    gimnasia, y para unir los 2, practico tambien el bien aclamado y popular deporte, crossfit, lo cual despues de 3 años, soy considerado
+                    un atleta amateur paraguayo en las disciplinas del crossfit, que al igual, en levantamiento olimpico. El entrenamiento llega a ser muy dificil,
+                    inclusive, infernal, pero incluisve, despues de todo ese "sufrimiento", los atletas siguen adelante, por que ellos siempre tienen en mente, un objetivo
+                    personal en el cual quieren cumplir.
+                </p>
+              <div class="heroes">
+                <h2>Los heroes del deporte</h2>
+                <h5></h5>
+                <div class="trinidad" >
+                  <img class="imagen" src="https://morningchalkup.com/wp-content/uploads/2018/03/froning-fraser-bridges-1440xauto@2x.jpg" class="trinidad"/>
+                  </div>
+                <p>estos 3 hombres (de izquierda a derecha: Rich Froning, Mathew Fraser y Josh Bridges) son considerados como los mejores atletas dentro del crossfit
+                    sin duda, hoy en día por 3 años consecutivos, Mat, es el mejor del mundo
+                </p>
+              </div>
+              <div class="myself">
+                <h3>Datos curiosos mios...</h3>
+                <li>Vivo con mis abuelos en luque, mientras que mis padres viven en chile</li>
+                <li>Vivo en una familia de 5 personas aproximadamente</li>
+                <li>una vez que termine la falcultad, tengo pensado estudiar cocina, y si es posible, en Francia</li>
+                <footer>
+          <div class="footer">
+            <p>autor: Ricardo Romero <br/>
+            gracias por visitar este blog
+            </p>
+          </div>
+          </footer>
+              </div>
+            </div>
+            </div>
+            </body>
+          </>
+        )
+      }
+    }
+export default Main;
